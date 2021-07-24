db593b8f7d19cd6e5b41c6597a9ffcbce42533c5
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-07-24 08:31:17 -0700

    adds solution for min max

--- lessons/06-min-max.test.js
+++ lessons/06-min-max.test.js
@@ -1,1 +1,39 @@
 // `min` and `max` with `reduce`
+
+function min(numbers) {
+  return numbers.reduce(
+    (acc, number) => (number < acc ? number : acc),
+    Infinity
+  )
+}
+
+describe('min', () => {
+  it('should return the lowest number in the list', () => {
+    expect(min([1, 3, 2])).toEqual(1)
+    expect(min([6, 0, 4])).toEqual(0)
+    expect(min([-1, -5, -2])).toEqual(-5)
+  })
+
+  it('should return Infinity if provided an empty array', () => {
+    expect(min([])).toEqual(Infinity)
+  })
+})
+
+function max(numbers) {
+  return numbers.reduce(
+    (acc, number) => (number > acc ? number : acc),
+    -Infinity
+  )
+}
+
+describe('max', () => {
+  it('should return -Infinity if provided an empty array', () => {
+    expect(max([])).toEqual(-Infinity)
+  })
+
+  it('should return the highest number in the list', () => {
+    expect(max([1, 3, 2])).toEqual(3)
+    expect(max([6, 0, 4])).toEqual(6)
+    expect(max([-1, -5, -2])).toEqual(-1)
+  })
+})

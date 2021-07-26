1501b7b1be9005c76d5d6672479272f6eb1610af
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-07-26 11:01:33 -0700

    adds solution for index by key

--- lessons/15-index-by-key.test.js
+++ lessons/15-index-by-key.test.js
@@ -1,1 +1,26 @@
 // Practical use: `indexByKey`
+
+function indexByKey(items, key) {
+  return items.reduce((acc, item) => {
+    acc[item[key]] = item
+    return acc
+  }, {})
+}
+
+describe('indexByKey', () => {
+  it('should index by key', () => {
+    const items = [
+      { id: 'asdf', name: 'Kyle' },
+      { id: 'hjkl', name: 'Anna' },
+    ]
+
+    expect(indexByKey(items, 'id')).toEqual({
+      asdf: { id: 'asdf', name: 'Kyle' },
+      hjkl: { id: 'hjkl', name: 'Anna' },
+    })
+    expect(indexByKey(items, 'name')).toEqual({
+      Kyle: { id: 'asdf', name: 'Kyle' },
+      Anna: { id: 'hjkl', name: 'Anna' },
+    })
+  })
+})

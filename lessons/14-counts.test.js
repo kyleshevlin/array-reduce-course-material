91cef757d419c77f02b6ee406cd90077885a26e9
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-07-26 10:57:56 -0700

    add solution for counts

--- lessons/14-counts.test.js
+++ lessons/14-counts.test.js
@@ -1,1 +1,24 @@
 // Practical use: `counts`
+
+function counts(items) {
+  return items.reduce((acc, cur) => {
+    if (!acc[cur]) {
+      acc[cur] = 0
+    }
+
+    acc[cur]++
+
+    return acc
+  }, {})
+}
+
+describe('counts', () => {
+  it('should count values', () => {
+    expect(counts([1, 2, 2, 3, 3, 3, 4, 4, 4, 4])).toEqual({
+      1: 1,
+      2: 2,
+      3: 3,
+      4: 4,
+    })
+  })
+})

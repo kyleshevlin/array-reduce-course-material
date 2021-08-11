7d791439217578c8a2efcd4cdfe4ef8711cd0642
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-08-11 09:08:00 -0700

    tidy up join

--- lessons/07-join.test.js
+++ lessons/07-join.test.js
@@ -4,7 +4,7 @@ function join(items, separator = ',') {
   return items.reduce((acc, item) => {
     item = String(item)
 
-    return acc ? `${acc}${separator}${item}` : item
+    return acc ? acc + separator + item : item
   }, '')
 }
 
@@ -13,15 +13,15 @@ describe('join', () => {
     expect(join([])).toEqual('')
   })
 
-  it('should coerce values to strings', () => {
-    expect(join([1])).toEqual('1')
-  })
-
-  it('should concatenate the string w/ a separator between each item', () => {
+  it('should join the string w/ a separator between each item', () => {
     expect(join(['a', 'b', 'c'], '-')).toEqual('a-b-c')
   })
 
-  it('should use `,` as the default separator', () => {
+  it('should use a comma as the default separator', () => {
     expect(join(['a', 'b', 'c'])).toEqual('a,b,c')
+  })
+
+  it('should coerce values to strings', () => {
+    expect(join([1])).toEqual('1')
   })
 })
